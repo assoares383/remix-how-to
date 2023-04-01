d1c073fa6352fc405c746242213b7a8b05f1aa98
= <alexandressoares383@gmail.com>
2023-04-01 15:26:14 -0300

    feat: como trabalhar com variaveis de ambiente

---
+++ 2-como-trabalhar-com-variaveis-de-ambiente/app/root.tsx
@@ -0,0 +1,44 @@
+import { json } from "@remix-run/node";
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+  useLoaderData,
+} from "@remix-run/react";
+
+export function loader() {
+  return json({
+    ENV: {
+      TIMEOUT: ENV.TIMEOUT,
+    },
+  });
+}
+
+export default function App() {
+  const { ENV } = useLoaderData<typeof loader>();
+
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.ENV = ${JSON.stringify(ENV)}`,
+          }}
+        />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
