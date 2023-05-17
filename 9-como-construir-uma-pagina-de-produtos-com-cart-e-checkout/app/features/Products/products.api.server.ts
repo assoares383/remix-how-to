07b5e3cdb9dcddedc3283ece04f9335f21ea9282
= <alexandressoares383@gmail.com>
2023-05-17 16:10:28 -0300

    feat: add action

--- 9-como-construir-uma-pagina-de-produtos-com-cart-e-checkout/app/features/Products/products.api.server.ts
+++ 9-como-construir-uma-pagina-de-produtos-com-cart-e-checkout/app/features/Products/products.api.server.ts
@@ -3,3 +3,9 @@ import { db } from "~/db";
 export function getProducts() {
   return db.product.findMany();
 }
+
+export function getProduct({ id }: { id: number }) {
+  return db.product.findUnique({
+    where: { id },
+  });
+}
--- 9-como-construir-uma-pagina-de-produtos-com-cart-e-checkout/app/routes/products.tsx
+++ 9-como-construir-uma-pagina-de-produtos-com-cart-e-checkout/app/routes/products.tsx
@@ -1,7 +1,18 @@
-import { ProductList, getProducts } from "~/features/Products";
+import { ProductList, getProduct, getProducts } from "~/features/Products";
 import { json, type LoaderArgs } from "@remix-run/node";
-import { useLoaderData } from "react-router";
+import { useLoaderData } from "@remix-run/react";
 import { Cart } from "~/features/Cart";
+import type { ActionArgs } from "@remix-run/node";
+
+export async function action({ request }: ActionArgs) {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+  const product = await getProduct({ id: Number(data.productId) });
+
+  console.log("product", product);
+
+  return null;
+}
 
 export async function loader({ request }: LoaderArgs) {
   const products = await getProducts();
